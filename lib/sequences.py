4f1b2bef77cb610a483b19fbaba001ed032dd2a7
Nyakio-Eva <evalkariuki@gmail.com>
2024-02-29 16:46:33 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+
+    if length == 0:
+        print([])
+
+    elif length == 1:
+        print([0])
+
+    elif length == 2:
+        fibonacci = [0,1]
+        print(fibonacci)        
+    
+    else:
+        fibonacci = [0,1]
+        for i in range(2, length):
+            c = fibonacci [-1] + fibonacci[-2]
+            fibonacci.append(c)
+        print(fibonacci)    
+        
+        
+print_fibonacci(10)                       
